ce37cf36f684f27e7c850696c8439055aa25ff8b
Ayola-Senzo-2 <ivyinamy2@gmail.com>
2024-09-03 11:50:19 +0200

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomAuth.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Entities/UserAccount.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace CustomAuth.Entities
+{
+    [Index(nameof(Email), IsUnique = true)]
+    [Index(nameof(Username), IsUnique = true)]
+    public class UserAccount
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "First name is required.")]
+        [MaxLength(50, ErrorMessage = "Max character allowed is 50.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Last name is required.")]
+        [MaxLength(50,ErrorMessage = "Max character allowed is 50")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Email is required.")]
+        [MaxLength(100, ErrorMessage = "Max character allowed is 100.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Username is required.")]
+        [MaxLength(20,ErrorMessage = "Max character allowed is 20.")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [MaxLength(20, ErrorMessage = "Max 20 allowed characters allowed.")]
+        public string Password { get; set; }
+
+    }
+}
